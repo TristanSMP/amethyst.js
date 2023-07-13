ccc604f9bcebe894270126cca63c4e57d6579aa0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-07-13 02:47:21 +0000

    Release 0.0.26

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as Amethyst from "./api";
 export { AmethystClient } from "./Client";
+export { AmethystEnvironment } from "./environments";
 export { AmethystError, AmethystTimeoutError } from "./errors";
